7d6b1f0f7c9e06e96cf929755bb63e317ba5a65c
Angel <info@angelfrancisco.es>
2016-12-17 01:27:13 +0100

    Crear tabla de elementos multimedia

---
+++ database/migrations/2016_12_17_011000_create_media_tables.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+/**
+ * Class CreateMediaTables
+ */
+class CreateMediaTables extends Migration
+{
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('media', function (Blueprint $table) {
+			$table->increments('id')->unsigned();
+			$table->string('title');
+			$table->string('description')->nullable();
+			$table->integer('user_id');
+			$table->string('type');
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::dropIfExists('media');
+	}
+}
