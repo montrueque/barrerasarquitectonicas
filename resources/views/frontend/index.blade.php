e8dc43bf11c1bc0cccb287b8166c299b13ba759f
Angel <info@angelfrancisco.es>
2016-12-17 10:30:07 +0100

    Aplicar Google Maps en pagina de inicio

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -23,17 +23,14 @@
 
 @section('after-scripts')
 
-    <script async defer src="https://maps.googleapis.com/maps/api/js?callback=initMap"></script>
+    <script src="https://maps.googleapis.com/maps/api/js?key= AIzaSyCqveAbp5MaCFiQ8bfD72epubvrJOD2jE4&callback=initMap" async defer></script>
+
     <script>
         function initMap() {
-            var uluru = {lat: -25.363, lng: 131.044};
             var map = new google.maps.Map(document.getElementById('map'), {
-                zoom: 4,
-                center: uluru
-            });
-            var marker = new google.maps.Marker({
-                position: uluru,
-                map: map
+                center: {lat: -34.397, lng: 150.644},
+                scrollwheel: false,
+                zoom: 8
             });
         }
     </script>
