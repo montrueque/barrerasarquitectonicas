1335e7c53dac6a0473ce0cac4aae7f025c3e53ad
Angel <info@angelfrancisco.es>
2016-12-17 04:24:31 +0100

    Actualizar frontend controller

--- app/Http/Controllers/Frontend/FrontendController.php
+++ app/Http/Controllers/Frontend/FrontendController.php
@@ -34,8 +34,8 @@ class FrontendController extends Controller
 	 */
 	public function concienciaSocial()
 	{
-		$testimonios = Media::where('type', 'testimonio')->take(5)->get();
-		$barreras = Media::where('type', 'barrera')->take(5)->get();
+		$testimonios = Media::where('type', '=', 'testimonio')->take(5)->get();
+		$barreras = Media::where('type', '=', 'barrera')->take(5)->get();
 		
 		return view('frontend.pages.conciencia_social')->with(compact('media', 'barreras'));
 	}
