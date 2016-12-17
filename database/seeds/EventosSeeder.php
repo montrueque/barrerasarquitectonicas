82b06205c783a71a80153d97d1cf45484ea8072d
Angel <info@angelfrancisco.es>
2016-12-17 02:51:52 +0100

    Corregidos EventosSeeder y LeyesSeeder

--- database/seeds/EventosSeeder.php
+++ database/seeds/EventosSeeder.php
@@ -1,9 +1,9 @@
 <?php
 
-use \Illuminate\Database\Seeder
-use \Illuminate\Support\Facades\DB
-use \App\Evento
-use \Carbon\Carbon
+use \Illuminate\Database\Seeder;
+use \Illuminate\Support\Facades\DB;
+use \App\Evento;
+use \Carbon\Carbon;
 
 class EventosSeeder extends Seeder {
     public function run()
--- database/seeds/LeyesSeeder.php
+++ database/seeds/LeyesSeeder.php
@@ -1,9 +1,9 @@
 <?php
 
-use \Illuminate\Database\Seeder
-use \Illuminate\Support\Facades\DB
-use \App\Ley
-use \Carbon\Carbon
+use \Illuminate\Database\Seeder;
+use \Illuminate\Support\Facades\DB;
+use \App\Ley;
+use \Carbon\Carbon;
 
 class LeyesSeeder extends Seeder {
     public function run()
